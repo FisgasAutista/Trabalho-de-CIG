d1d1521b9eabb614548552c4b932bece6aa0ba1b
Areias <paulocarlaoareias@gmail.com>
2019-02-18 16:14:24 +0000

    Updte all tables , add sort + search

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,7 @@
 //pagetabs
 
+//plantel
+
 function openPage(pageName,elmnt,color) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -245,6 +247,30 @@ function sortTable3(n)
       if (flag == 0 && resposta == "ascendente") {
         resposta = "descendente";
         trocar = true;
+      }
+    }
+  }
+}
+
+
+
+function mySearch() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
       }
     }
   }
